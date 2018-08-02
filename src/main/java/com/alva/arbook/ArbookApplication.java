7c9cbd9a0fb56284c4610a16b70fdc1c56ff78c9
cottoncandies <1790882721@qq.com>
2018-08-02 17:15:08 +0800

    版本2.0

--- src/main/java/com/alva/arbook/ArbookApplication.java
+++ src/main/java/com/alva/arbook/ArbookApplication.java
@@ -3,17 +3,11 @@ package com.alva.arbook;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("com.alva.arbook.dao")
 public class ArbookApplication extends SpringBootServletInitializer{
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(ArbookApplication.class);
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(ArbookApplication.class, args);
     }
--- src/test/java/com/alva/arbook/controller/SysSubjectTControllerTest.java
+++ src/test/java/com/alva/arbook/controller/SysSubjectTControllerTest.java
@@ -1,10 +1,6 @@
 package com.alva.arbook.controller;
 
-import com.alva.arbook.dao.SysTextbookTMapper;
-import com.alva.arbook.service.SysTextbookTService;
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -13,14 +9,5 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SysSubjectTControllerTest {
 
-    @Autowired
-    private SysTextbookTService sysTextbookTService;
-    @Autowired
-    private SysTextbookTMapper sysTextbookTMapper;
 
-
-    @Test
-    public void insert() {
-
-    }
 }
