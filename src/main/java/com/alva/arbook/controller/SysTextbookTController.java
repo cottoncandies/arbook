166dffbea674f0befac1b6b13cba497bb64acd26
cottoncandies <1790882721@qq.com>
2018-08-10 18:08:44 +0800

    textbook表增加term字段,调整上传文件代码逻辑

--- src/main/java/com/alva/arbook/controller/SysTextbookTController.java
+++ src/main/java/com/alva/arbook/controller/SysTextbookTController.java
@@ -4,21 +4,22 @@ import com.alva.arbook.entity.SysTextbookT;
 import com.alva.arbook.service.AppKeyTService;
 import com.alva.arbook.service.SysTextbookTService;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
-import java.io.*;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
 
-@Controller
+@RestController
 @RequestMapping("/ApiV1")
 public class SysTextbookTController {
 
@@ -33,13 +34,17 @@ public class SysTextbookTController {
 
     @Value("${baseUploadPath}")
     private String baseUploadPath;
-    
+
     //根据条件查询教材(联表查询)
     @RequestMapping("/GetIndex")
-    @ResponseBody
-    public Map GetIndex(@RequestParam("ak") String accessKey, String subject, String publish, String section, String grade, @RequestParam("page") int page, @RequestParam("limit") int limit) {
+    public Map GetIndex(@RequestParam("ak") String accessKey,
+                        String subject,
+                        String publish,
+                        String section,
+                        String grade,
+                        @RequestParam("page") int page,
+                        @RequestParam("limit") int limit) {
         Map<String, Object> map = new HashMap<>();
-        //判断授权Key是否合法
         if (appKeyTService.selectByAccessKey(accessKey) != null) {
             List<SysTextbookT> textbookTS = sysTextbookTService.selectByCustom(subject, publish, section, grade, page, limit);
             map.put("code", 0);//查询状态
@@ -54,18 +59,11 @@ public class SysTextbookTController {
         return map;
     }
 
-    /*文件导出的核心代码
-    File file = new File("E:/wzh/book/初中语文1.zip");
-    String path = "E:/wzh/aa.zip";
-    File desFile = new File(path);
-    FileUtils.copyFile(file, desFile);
-    */
     @RequestMapping("/exportBookList")
-    @ResponseBody
-    public Map exportBookList(@RequestParam(value = "bookIds[]") String[] bookIds) throws IOException {
+    public Map exportBookList(@RequestParam(value = "bookIds[]") String[] bookIds,
+                              String directory) throws IOException {
         Map<String, Object> map = new HashMap<>();
-
-        //确保导出目录文件夹的唯一性(命名采用导出目录/学校/导出时间)
+        //确保导出目录文件夹的唯一性
         String exportPath = baseExportPath;
         File exportFile = new File(exportPath);
         //创建导出目录文件夹
@@ -85,146 +83,75 @@ public class SysTextbookTController {
                 map.put("export", "文件" + fileName + "不存在");
             }
         }
-        map.put("msg", "所有教材已导出到目录" + exportPath);
-
+        map.put("msg", "教材已导出到目录" + exportPath);
         return map;
     }
 
-
     // 单文件上传
-    @RequestMapping("/upload")
-    @ResponseBody
+    @PostMapping(value = "/upload")
     public Map upload(@RequestParam("file") MultipartFile file) {
+        /*
+        文件上传做md5
+        uuid防止上传文件重名
+        文件大小
+        原文件名
+        将信息存入数据库
+        */
+
+
+//        "id": "5ab9ee053db2b526f810a1bd",
+//                "cover":"0000.jpg",
+//                "caption": "八年级物理下",
+//                "edition": "2013版",
+//                "section": "初中",
+//                "grade": "八年级",
+//                "subject": "物理",
+//                "term": "下学期",
+//                "publish": "人民教育出版社",
+        //读取教材文件中的json文件获取上面的属性
         Map<String, Object> map = new HashMap<>();
         try {
-            if (file.isEmpty()) {
-                map.put("msg", "文件为空");
-                return map;
-            }
-            // 获取文件名
-            String fileName = file.getOriginalFilename();
-            // 获取文件的后缀名
-            String suffixName = fileName.substring(fileName.lastIndexOf("."));
-
-            // 设置文件存储路径
-            String filePath = "E://wzh//";
-            String path = filePath + fileName;
-
+            String fileName = file.getOriginalFilename();// 获取文件名
+            String extension = FilenameUtils.getExtension(fileName);// 获取文件的后缀名
+            String newFileName = UUID.randomUUID().toString().replaceAll("-", "") + "." + extension;//文件重命名
+            String path = baseUploadPath + "/" + newFileName;
             File dest = new File(path);
-            // 检测是否存在目录
             if (!dest.getParentFile().exists()) {
                 dest.getParentFile().mkdirs();// 新建文件夹
             }
             file.transferTo(dest);// 文件写入
+
+            SysTextbookT textbook = new SysTextbookT("aaaaazzzzzzz","yuwen","000.jpg","diyiban",
+                    "xioaxue","sannianji",1,"1","1",
+                    path,"md5",file.getSize());
+            sysTextbookTService.insert(textbook);
             map.put("msg", "上传成功");
-            return map;
-        } catch (IllegalStateException e) {
+        } catch (Exception e) {
             e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+            map.put("msg", "上传失败");
         }
-        map.put("msg", "上传失败");
         return map;
     }
 
-    //获取教材文件封面
-    @RequestMapping(value = "/GetCover")
-    public void getCover(HttpServletResponse res, String ak, @RequestParam("id") String bookId) {
-        //1判断授权Key是否合法
-        if (ak != null && ak != "" && appKeyTService.selectByAccessKey(ak) != null) {
-            //2查询下载文件封面的信息
-            SysTextbookT sysTextbookT = sysTextbookTService.selectByPrimaryKey(bookId);
-            String fileCoverName = sysTextbookT.getSzCover();// 查询文件封面名
-            String realPath = sysTextbookT.getSzStore();// 查询存放位置
-            File file = new File(realPath, fileCoverName);
-            //3判断文件是否存在
-            if (file.exists()) {
-                byte[] buff = new byte[1024];
-                BufferedInputStream bis = null;
-                OutputStream os = null;
-                //4下载文件封面
-                try {
-                    res.setContentType("application/octet-stream");
-                    res.setHeader("content-type", "application/octet-stream");
-                    res.setHeader("Content-Disposition", "attachment;filename=" + new String(fileCoverName.getBytes("utf-8"), "ISO8859-1"));
-                    res.addHeader("Content-Length", String.valueOf(file.length()));
-                    res.addHeader("File-Name", new String(fileCoverName.getBytes("utf-8"), "ISO8859-1"));
-                    os = res.getOutputStream();
-                    bis = new BufferedInputStream(new FileInputStream(file));
-                    int i;
-                    while (-1 != (i = bis.read(buff))) {
-                        os.write(buff, 0, buff.length);
-                    }
-                    os.flush();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } finally {
-                    if (os != null) {
-                        try {
-                            os.close();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                    if (bis != null) {
-                        try {
-                            bis.close();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    //获取教材文件
+    //获取教材
     @RequestMapping(value = "/GetData")
-    public void getData(HttpServletResponse res, String ak, @RequestParam("id") String bookId) {
-        //1判断授权Key是否合法
+    public void getData(HttpServletResponse response, String ak, @RequestParam("id") String bookId) throws IOException {
+        //授权Key是否合法
         if (ak != null && ak != "" && appKeyTService.selectByAccessKey(ak) != null) {
-            //2查询下载文件的信息
+            //下载文件的信息
             SysTextbookT sysTextbookT = sysTextbookTService.selectByPrimaryKey(bookId);
             String fileName = sysTextbookT.getSzCaption();// 查询文件名
             String realPath = sysTextbookT.getSzStore();// 查询文件存放位置
             File file = new File(realPath, fileName);
-            //3判断文件是否存在
             if (file.exists()) {
-                byte[] buff = new byte[1024];
-                BufferedInputStream bis = null;
-                OutputStream os = null;
-                //4下载文件封面
-                try {
-                    res.setContentType("application/octet-stream");
-                    res.setHeader("content-type", "application/octet-stream");
-                    res.setHeader("Content-Disposition", "attachment;filename=" + new String(fileName.getBytes("utf-8"), "ISO8859-1"));
-                    res.addHeader("Content-Length", String.valueOf(file.length()));
-                    res.addHeader("File-Name", new String(fileName.getBytes("utf-8"), "ISO8859-1"));
-                    os = res.getOutputStream();
-                    bis = new BufferedInputStream(new FileInputStream(file));
-                    int i;
-                    while (-1 != (i = bis.read(buff))) {
-                        os.write(buff, 0, buff.length);
-                    }
-                    os.flush();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } finally {
-                    if (os != null) {
-                        try {
-                            os.close();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                    if (bis != null) {
-                        try {
-                            bis.close();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
+                //下载文件
+                response.setContentType("application/octet-stream");
+                response.setHeader("content-type", "application/octet-stream");
+                response.setHeader("Content-Disposition", "attachment;filename=" + new String(fileName.getBytes("utf-8"), "ISO8859-1"));
+                response.addHeader("Content-Length", String.valueOf(file.length()));
+                response.addHeader("File-Name", new String(fileName.getBytes("utf-8"), "ISO8859-1"));
+                ServletOutputStream outputStream = response.getOutputStream();
+                outputStream.write(FileUtils.readFileToByteArray(file));
             }
         }
     }
