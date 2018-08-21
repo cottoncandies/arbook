db8dda943d661cfc58bcd8ec173cd5aa1acca6d4
cottoncandies <1790882721@qq.com>
2018-08-21 13:57:47 +0800

    用户登录认证

---
+++ src/main/java/com/alva/arbook/config/MvcConfig.java
@@ -0,0 +1,29 @@
+package com.alva.arbook.config;
+
+import com.alva.arbook.interceptor.LoginInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Autowired
+    private LoginInterceptor loginInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**")    // 拦截配置
+                .excludePathPatterns("/error") // 排除配置
+                .excludePathPatterns("/login**");
+    }
+
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/*/**").addResourceLocations("classpath:/webapp/");
+//        super.addResourceHandlers(registry);
+//    }
+}
---
+++ src/main/java/com/alva/arbook/interceptor/LoginInterceptor.java
@@ -0,0 +1,29 @@
+package com.alva.arbook.interceptor;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@Component
+public class LoginInterceptor extends HandlerInterceptorAdapter {
+
+    @Value("${sessionLoginUser}")
+    private String sessionLoginUser;
+
+//    @Override
+//    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+//            throws Exception {
+//        HttpSession session = request.getSession();
+//        if (session.getAttribute(sessionLoginUser) != null) {
+//            return true;
+//        } else {
+//            String url = request.getContextPath() + "/login.html";
+//            response.sendRedirect(url);
+//            return false;
+//        }
+//    }
+}
